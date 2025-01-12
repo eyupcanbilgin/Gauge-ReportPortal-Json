ea27a7318be7ac994a3204c4233f32d0c3a69cdf
eyupcanbilgin <eyupcanbilgin01@gmail.com>
2025-01-12 06:51:46 +0300

    TransferPage ve TransferSteps geliştirmeler yapıldı.

--- src/test/java/com/banking/stepImp/TransferSteps.java
+++ src/test/java/com/banking/stepImp/TransferSteps.java
@@ -1,4 +1,23 @@
 package com.banking.stepImp;
 
-public class TransferSteps{
+import com.banking.pages.TransferPage;
+import com.thoughtworks.gauge.Step;
+
+public class TransferSteps {
+
+    private final TransferPage transferPage;
+
+    public TransferSteps() {
+        this.transferPage = new TransferPage();
+    }
+
+    @Step("Click the transfer money button")
+    public void clickTransferMoneyButton() {
+        transferPage.clickTransferMoneyButton();
+    }
+
+    @Step("Verify transfer money popup elements is visible")
+    public void verifyTransferMoneyPopupElements() {
+        transferPage.verifyTransferMoneyPopupElements();
+    }
 }
